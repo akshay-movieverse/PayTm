e96aaf8e97f2791d7b832536d82cd2878255b443
akshay-movieverse <akshayrameshwar2018@gmail.com>
2025-05-11 17:35:54 +0530

    4th commit

--- paytm/settings.py
+++ paytm/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-^e&25i%5e117542_33jiv&@j$7-c_q!&vfo_*p$&jnt3aqm=f+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
